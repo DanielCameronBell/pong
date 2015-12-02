9cad362f1133cca6dd473f28fcf1584595137fd3
Alex Bell <tabell@uvic.ca>
2015-12-01 20:02:01 -0800

    added a few comments and deleted a few blank lines

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
+// this is a comment added by alex
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <SDL_INIT_EVERYTHING>
+#include <SDL_INIT_EVERYTHING> // <-- pretty sure this isn't right
 using namespace std;
-
-
 int main(){
-	
-
 return 0;
 }
